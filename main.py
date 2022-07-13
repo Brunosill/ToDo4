9163e248fdfc944d37d3ed85edb2c3246ed1a117
Bruno Guedes Da Silva <71763403+Brunosill@users.noreply.github.com>
2022-07-12 22:06:30 -0300

    ja vai

--- main.py
+++ main.py
@@ -1,15 +1,37 @@
 lista_candidatos = []
 
-def adicionarCandidatos(Nome, Resumo):
-    teste_analita = ['Python', 'PowerBI', 'Banco de dados', 'SQL', 'Boa comunicaçao']
-    tests_cientista = [ 'Python', 'Banco de dados', 'Machine Learning','Resolução De Problemas', 'Estatística']
+def main():
+    quantidadeCandidados = int(input("Quantos candidados deseja cadastra: "))
+
+    for i in range(quantidadeCandidados):
+        adicionarCandidatos()
+
+        quantidadeCandidados = quantidadeCandidados -1
+
+    arquivo = ('candidatos.txt', 'w')
+    for x in lista_candidatos:
+        arquivo.write(x)
+    arquivo.close()
+    print("Salvo em TXT")
+
+def adicionarCandidatos():
+    teste_analita = ['PowerBI', 'SQL', 'Boa comunicaçao']
+    teste_cientista = ['Machine Learning','Resolução De Problemas', 'Estatística']
     
-    candidato = {'Nome': '', "Resumo":'', 'Vaga': ''}
+    candidato = {'Nome': '', "Resumo":'', 'Vaga': '0'}
     
-    for i in candidato.keys():
-        if(i == 'Nome'):
-            candidato['Nome'].append(Nome)
-        elif(i == 'Resumo'):
-            candidato['Resumo'].append(Resumo)
-        elif(i == 'Vaga'):
-            if(Resumo.find(teste_analita[1]) != -1) or lista_candidatos = []
+    candidato['Nome'] = input("Nome do candidato: ")
+    candidato['Resumo']= input("Resumo do candidato: ")
+
+    for x in teste_analita:
+        if(candidato["Resumo"].lower().find(x.lower()) != -1):
+            candidato['Vaga'] = "Análista de dados"
+    for x in teste_cientista:
+        if(candidato['Resumo'].lower().find(x.lower()) != -1):
+            candidato['Vaga'] = "Cientista de dados"
+    if(candidato['Vaga'] == 0):
+        candidato['Vaga'] = 'Não se qualificou pra nem uma vaga!'
+
+    print(candidato)
+
+main()
